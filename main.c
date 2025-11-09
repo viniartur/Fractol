ee2a7f5103ca7322ea1c636ac111845c4e6fb004
viniartur <viniciusarturvieira@hotmail.com>
2025-11-09 20:34:25 -0300

    fractol

---
+++ main.c
@@ -0,0 +1,48 @@
+#include "fractol.h"
+
+int	main(int argc, char **argv)
+{
+	t_fractal	f;
+
+	if (argc < 2)
+		return (show_help(), 1);
+	if (argv[1][0] == 'M')
+		f.type = 1;
+	else if (argv[1][0] == 'J' && argc == 4)
+	{
+		f.type = 2;
+		f.julia_c.re = atof(argv[2]);
+		f.julia_c.im = atof(argv[3]);
+	}
+	else
+		return (show_help(), 1);
+	f.mlx = mlx_init();
+	f.win = mlx_new_window(f.mlx, WIDTH, HEIGHT, "fract'ol");
+	f.img = mlx_new_image(f.mlx, WIDTH, HEIGHT);
+	f.addr = mlx_get_data_addr(f.img, &f.bits_per_pixel,
+			&f.line_length, &f.endian);
+	f.zoom = 1.0;
+	f.offset_x = 0.0;
+	f.offset_y = 0.0;
+	mlx_key_hook(f.win, key_hook, &f);
+	mlx_mouse_hook(f.win, mouse_hook, &f);
+	mlx_hook(f.win, 17, 0, close_window, &f);
+	render_fractal(&f);
+	mlx_loop(f.mlx);
+	return (0);
+}
+
+void	show_help(void)
+{
+	write(1, "Usage:\n", 7);
+	write(1, "  ./fractol M          - Mandelbrot\n", 36);
+	write(1, "  ./fractol J -0.7 0.27 - Julia with custom parameters\n", 53);
+}
+
+int	close_window(t_fractal *f)
+{
+	mlx_destroy_image(f->mlx, f->img);
+	mlx_destroy_window(f->mlx, f->win);
+	exit(0);
+	return (0);
+}
