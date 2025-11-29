08ef68617a61c1d2ce66c3fe7d4d3fcdc55a655d
vvieira <viniciusarturvieira@hotmail.com>
2025-11-29 11:45:25 -0300

    atualização do complex

--- complex.c
+++ complex.c
@@ -6,7 +6,7 @@
 /*   By: vvieira <vvieira@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 11:11:35 by vvieira           #+#    #+#             */
-/*   Updated: 2025/11/29 11:24:07 by vvieira          ###   ########.fr       */
+/*   Updated: 2025/11/29 11:45:05 by vvieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,37 @@ t_complex	complex_square(t_complex a)
 double	complex_abs(t_complex a)
 {
 	return (sqrt(a.re * a.re + a.im * a.im));
+}
+
+int	calculate_mandelbrot(t_complex c)
+{
+	t_complex	z;
+	int			iter;
+
+	z.re = 0;
+	z.im = 0;
+	iter = 0;
+	while (iter < MAX_ITER)
+	{
+		z = complex_add(complex_square(z), c);
+		if (complex_abs(z) > 2.0)
+			break ;
+		iter++;
+	}
+	return (iter);
+}
+
+int	calculate_julia(t_complex z, t_complex c)
+{
+	int	iter;
+
+	iter = 0;
+	while (iter < MAX_ITER)
+	{
+		z = complex_add(complex_square(z), c);
+		if (complex_abs(z) > 2.0)
+			break ;
+		iter++;
+	}
+	return (iter);
 }
